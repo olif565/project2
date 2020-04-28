31c33441bcd303fb649e43943545321d70b47838
irvanwahyu <irvanyale14@gmail.com>
2020-04-28 09:05:20 +0700

    training

--- home/views/training.py
+++ home/views/training.py
@@ -5,21 +5,21 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def get_training():
+def get_matriks():
 
     n_data_normalisasi = kernel.get_kernel()['n_data_normalisasi']
     n_list_data_kernel = kernel.get_kernel()['n_list_data_kernel']
     n_list_data_kernel_view = kernel.get_kernel()['n_list_data_kernel_view']
 
-    n_list_data_training = []
-    n_list_data_training_view = []
+    n_list_data_matriks = []
+    n_list_data_matriks_view = []
 
     # lambda
     l = 0.5
 
     for i, x in enumerate(n_list_data_kernel):
-        n_data_training = []
-        n_data_training_view = []
+        n_data_matriks = []
+        n_data_matriks_view = []
         y_i = int(n_data_normalisasi[i]['kelas'])
 
         for j, y in enumerate(x):
@@ -30,17 +30,68 @@ def get_training():
             d_ij = y_i * y_j * (y + (math.pow(l, 2)))
 
             if j == 0:
-                n_data_training_view.append(n_data_normalisasi[i]['no'])
+                n_data_matriks_view.append(n_data_normalisasi[i]['no'])
 
-            n_data_training_view.append(d_ij)
-            n_data_training.append(d_ij)
+            n_data_matriks_view.append(d_ij)
+            n_data_matriks.append(d_ij)
 
-        n_list_data_training.append(n_data_training)
-        n_list_data_training_view.append(n_data_training_view)
+        n_list_data_matriks.append(n_data_matriks)
+        n_list_data_matriks_view.append(n_data_matriks_view)
 
     data_kernel = {
         'n_data_normalisasi': n_data_normalisasi,
-        'n_list_data_training_view': n_list_data_training_view
+        'n_list_data_matriks': n_list_data_matriks,
+        'n_list_data_matriks_view': n_list_data_matriks_view
     }
 
     return data_kernel
+
+
+def get_error_rate(list_data_matriks):
+
+    # alpha
+    a = 0
+
+    data_error_rate = []
+
+    for i, x in enumerate(list_data_matriks):
+
+        for j, y in enumerate(x):
+
+            er = a * float(y)
+
+            if i == 0:
+                data_error_rate.append(er)
+            else:
+                data_error_rate[j] = data_error_rate[j] + er
+
+    error_rate = {
+        'data_error_rate': data_error_rate
+    }
+
+    return error_rate
+
+
+def get_delta_alfa(data_error_rate):
+
+    # alpha
+    a = 0
+
+    # constant
+    const = 1
+
+    # gamma
+    g = 0.01
+
+    data_delta_alfa = []
+
+    for i, x in enumerate(data_error_rate):
+
+        da = min(max(g * (1 - x), -a), (const - a))
+        data_delta_alfa.append(da)
+
+    delta_alfa = {
+        'data_delta_alfa': data_delta_alfa
+    }
+
+    return delta_alfa
--- home/views/view_training.py
+++ home/views/view_training.py
@@ -10,12 +10,17 @@ class IndexView(ListView):
     context_object_name = 'data'
 
     def get_queryset(self):
-        n_data_normalisasi = training.get_training()['n_data_normalisasi']
-        n_list_data_training_view = training.get_training()['n_list_data_training_view']
+        n_data_normalisasi = training.get_matriks()['n_data_normalisasi']
+        n_list_data_matriks = training.get_matriks()['n_list_data_matriks']
+        n_list_data_matriks_view = training.get_matriks()['n_list_data_matriks_view']
+        data_error_rate = training.get_error_rate(n_list_data_matriks)['data_error_rate']
+        data_delta_alfa = training.get_delta_alfa(data_error_rate)['data_delta_alfa']
 
         context = {
             'n_data_normalisasi': n_data_normalisasi,
-            'n_list_data_training_view': n_list_data_training_view
+            'n_list_data_matriks_view': n_list_data_matriks_view,
+            'data_error_rate': data_error_rate,
+            'data_delta_alfa': data_delta_alfa
         }
 
         return context
