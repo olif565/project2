ed784b72a1dbe02017101ee7ad8959f9137baaf3
olif565 <skripsiapp98@gmail.com>
2020-06-17 16:11:25 +0700

    bootstrapLoginRegister

--- account/forms.py
+++ account/forms.py
@@ -4,29 +4,28 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=50,
-                               widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    first_name = forms.CharField(max_length=30,
+    first_name = forms.CharField(label='Fisrt Name', max_length=30,
                                  widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
-    last_name = forms.CharField(max_length=150,
+    last_name = forms.CharField(label='Last Name', max_length=150,
                                 widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
-    password1 = forms.CharField(max_length=30,
+    username = forms.CharField(label='Username', max_length=50,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password1 = forms.CharField(label='Password', max_length=30,
                                 widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    password2 = forms.CharField(max_length=30,
+    password2 = forms.CharField(label='Password Confirmation', max_length=30,
                                 widget=forms.PasswordInput(attrs={'placeholder': 'Password Confirmation'}))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(max_length=20,
+    username = forms.CharField(label='Username', max_length=20,
                                widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    password = forms.CharField(max_length=20,
+    password = forms.CharField(label='Password', max_length=20,
                                widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
     class Meta:
         model = User
         fields = ('username', 'password')
-
