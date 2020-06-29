a12f970dfea3d019ee5a17895a8d7d0a98b8d47f
olif565 <skripsiapp98@gmail.com>
2020-06-29 14:18:49 +0700

    hapusurl

--- ProjectSkripsi/urls.py
+++ ProjectSkripsi/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('account.urls', namespace = 'account')),
-    path('account/', include('account.urls', namespace='account')),
+    # path('account/', include('account.urls', namespace='account')),
     path('home/', include('home.urls', namespace='home'))
 ]
